27692c18901e9f9ee3ac1ab352a1f11c32925e69
Alish Bista <bistace321@gmail.com>
2023-08-30 16:29:56 +0545

    feat: express inititated

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('Task Manager App')
+// Importing modules
+const express = require('express');
+
+// starting the express app
+const app = express()
+
+// The index page
+app.get("/",(req,res) => {
+    res.send('Hello')
+})
+
+//Listen on port set in environment variable or default to 3000
+const listener = app.listen(process.env.PORT || 3000, function () {
+    console.log("Node.js listening on port " + listener.address().port);
+  });
+  
